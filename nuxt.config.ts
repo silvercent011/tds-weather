7430166af1c1ccb01af3909bec52e835cb10ee51
Sidney Alex <silvercenturion2@gmail.com>
2023-02-04 12:00:26 -0300

    feat(main): add environment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    public: {
+      API_KEY: process.env.API_KEY || "",
+      API_URL: process.env.API_URL || "",
+    },
+  },
+});
