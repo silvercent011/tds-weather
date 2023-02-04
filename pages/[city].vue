457d01c0c4bb093fa8ee69e5bb11c928fe0b0323
Sidney Alex <silvercenturion2@gmail.com>
2023-02-04 12:05:16 -0300

    feat(main): add city page

---
+++ pages/[city].vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>City</h1>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>Hello</h1>
+  <div>
+    <h1>Weather</h1>
+    <h1>Select a city</h1>
+    <div>
+      <NuxtLink
+        v-for="(city, index) in cities"
+        :key="index"
+        :href="city.toLowerCase()"
+      >
+        {{ city }}
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const cities = [
+  'Dallol', 'Fairbanks', 'London', 'Recife', 'Vancouver', 'Yakutsk'
+]
+</script>
