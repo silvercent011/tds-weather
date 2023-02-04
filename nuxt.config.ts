faa9758bdb24091022cbd92d9a79edb9b588d3a4
Sidney Alex <silvercenturion2@gmail.com>
2023-02-04 15:54:00 -0300

    feat(main): add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     public: {
       API_KEY: process.env.API_KEY || "",
