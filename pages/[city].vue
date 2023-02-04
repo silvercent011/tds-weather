df4fd4bd54263950d302d3f415e8dd9d1739800f
Sidney Alex <silvercenturion2@gmail.com>
2023-02-04 12:31:38 -0300

    feat(main): add logic to fetch data

--- pages/[city].vue
+++ pages/[city].vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>City</h1>
+  <div>
+    <div v-if="pending">LOADING</div>
+    <div v-else>
+      <pre>
+
+         {{ data }}
+        </pre
+      >
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { city } = useRoute().params;
+
+const { API_KEY, API_URL } = useRuntimeConfig().public;
+
+const { pending, data } = useLazyFetch(`${API_URL}/forecast.json`, {
+  params: {
+    key: API_KEY,
+    q: city,
+  },
+});
+</script>
